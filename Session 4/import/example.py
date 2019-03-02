a433223a3af1641fcc01421a1057ec5c71e48281
Aurelia Specker <aspecker@twitter.com>
2019-03-02 21:28:37 +0000

    Add material for session 4

---
+++ Session 4/import/example.py
@@ -0,0 +1,8 @@
+
+def addition(a, b):
+    result = a + b
+    return result  
+
+def subtraction(a, b):
+    result = a - b
+    return result
---
+++ Session 4/import/main.py
@@ -0,0 +1,22 @@
+import example
+
+print example.addition(1, 3)
+
+print example.subtraction(6, 3)
+
+# -------------------
+
+# import example as e
+
+# print e.addition(1, 3)
+
+# print e.subtraction(6, 3)
+
+# -------------------
+
+# from example import addition, subtraction
+
+# print addition(1, 3)
+
+# print subtraction(6, 3)
+
