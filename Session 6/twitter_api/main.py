0c3962a2380dcb32d5132348c2ffa6dd795c9e1d
Aurelia Specker <aspecker@twitter.com>
2019-03-16 16:21:04 +0000

    Add session 6

---
+++ Session 6/twitter_api/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask("Twitter")
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print form_data["email"]
+    return "All OK"
+
+app.run(debug=True)
